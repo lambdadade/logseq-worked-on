0cc21cdd5c97243e8150c7e2866c623844d13a22
Marc Seibert <marc.seibert@1und1.de>
2024-08-22 23:51:27 +0200

    add a date property for 'worked on' time instead of a timestamp for the done status

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 import "@logseq/libs";
-import * as dayjs from 'dayjs'
 
 
 const TASK_MARKERS = new Set(["DONE", "NOW", "LATER", "DOING", "TODO", "WAITING"]);
@@ -13,17 +12,21 @@ function main() {
 
     const hasProperty = taskBlock.properties?.doneMs;
 
-    if (taskBlock.marker === "DONE") {
+    if (taskBlock.marker === "NOW"  || taskBlock.marker === "DONE" || taskBlock.marker === "DOING" || taskBlock.marker === "CANCELLED" || taskBlock.marker === "WAITING") {
       if (hasProperty) {
         return;
       }
-      const now = dayjs();
-      logseq.Editor.upsertBlockProperty(taskBlock.uuid, "done-ms", now.valueOf());
+      // const now = dayjs();
+      const today = new Date();
+      const dd = String(today.getDate()).padStart(2, '0');
+      const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      const yyyy = today.getFullYear();
+      logseq.Editor.upsertBlockProperty(taskBlock.uuid, "last-worked-on", "[[" + yyyy + '_' + mm + '_' + dd + "]]");
     } else {
       if (!hasProperty) {
         return;
       }
-      logseq.Editor.removeBlockProperty(taskBlock.uuid, "done-ms");
+      // logseq.Editor.removeBlockProperty(taskBlock.uuid, "worked-on");
     }
   });
 }
